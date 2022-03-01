d163d875196a793ca59072c0686f1c3bc5fa19c3
Michele Cioccarelli <michele.cioccarelli04@gmail.com>
2022-03-01 12:30:22 +0100

    Added basic piece and board evaluation to Apium

--- apium/apium/apium.cpp
+++ apium/apium/apium.cpp
@@ -1,2 +1,54 @@
 #include "apium.h"
 
+float Apium::evaluate_piece(Coords pieceCoords) {
+    // Expects matrix-notation
+    float pieceEval = 0;
+    Piece tempPiece = engine.board.matrix[pieceCoords.row][pieceCoords.column].piece;
+
+    // There was no piece in the position
+    if (tempPiece.type == VUOTA) {
+        return pieceEval;
+    }
+
+     switch (playstyle) {
+        case NEUTRAL:
+        // EVALUATION TABLE FOR NEUTRAL
+        // DAMINA -> +1   | Increment based on position: +0.25
+        // DAMONA -> +3   | Increment based on position: +0.25
+            if (tempPiece.type == DAMA) {
+                if (tempPiece.color == BIANCO) {
+                    // White damina
+                    pieceEval++;
+
+                    // Add .25 based on how close it is to being a damone (1 square closer = +.25)
+                    pieceEval += pieceCoords.row * .25;
+                } else {
+                    // Black damina
+                    pieceEval--;
+
+                    // Same as above but inverted
+                    pieceEval -= (7 - pieceCoords.row) * .25;
+                }
+            } else {
+                pieceEval += 3;
+            }
+            break;
+         case AGGRESSIVE:
+         case DEFENSIVE:
+             break;
+    }
+    return pieceEval;
+}
+
+float Apium::get_eval() const { return eval; }
+
+float Apium::evaluate_current_position() {
+    float returnValue = 0;
+    // Fancy way of skipping white squares coming up
+    for (int row = 0; row < ROWS; row++) {
+        for (int col = (row % 2); col < COLUMNS; col += 2) {
+            returnValue += evaluate_piece(Coords((ColumnNotation)col, row));
+        }
+    }
+    return returnValue;
+}
--- apium/apium/apium.h
+++ apium/apium/apium.h
@@ -6,6 +6,8 @@
 #include "../playstyle/playstyle.h"
 
 class Apium {
+    float eval;
+
     // Used as a weight to calculate the position's score
     Playstyle playstyle;
 
@@ -13,11 +15,19 @@ class Apium {
     std::vector<Move> moves;
 
     // A list of all the pieces and their locations, so you don't have to look for the every time by looping the whole board
-    std::vector<Square> pieceCoords;
+    //std::vector<Square> pieceCoords;
 
     // Used for simulate_functions and access enemy logs
     GameEngine engine;
 public:
+    float get_eval() const;
+
+    // Returns the vsalue of the piece (positive for white pieces and negative for black ones)
+    float evaluate_piece(Coords pieceCoords);
+
+    // Evaluates the board stored in Apium's engine
+    float evaluate_current_position();
+
     PlayerColor playerColor;
 
     void evaluate_moves();
